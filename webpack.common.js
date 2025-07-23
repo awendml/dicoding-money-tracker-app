47dcfb16fb35f0d48df20eed9718aa1118a4214d
awendml <awendml@gmail.com>
2025-07-23 14:27:01 +0800

    finishing

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ const htmlWebpackPluginConfig = {
           <a class="nav-link" href="/">Dashboard</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Add Record</a>
+          <a class="nav-link" href="/transactions/add.html">Add Record</a>
         </li>
         <li class="nav-item dropdown d-none" id="userLoggedMenu">
           <a class="nav-link dropdown-toggle text-nowrap" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -78,6 +78,18 @@ module.exports = {
       title: 'Dashboard',
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/views/dashboard.html'),
+      ...htmlWebpackPluginConfig,
+    }),
+        new HtmlWebpackPlugin({
+      title: 'Add Records',
+      filename: 'transactions/add.html',
+      template: path.resolve(__dirname, 'src/views/transactions/add.html'),
+      ...htmlWebpackPluginConfig,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Edit Records',
+      filename: 'transactions/edit.html',
+      template: path.resolve(__dirname, 'src/views/transactions/edit.html'),
       ...htmlWebpackPluginConfig,
     }),
     new CopyWebpackPlugin({
